befc256bd453a7b5c7bc001391551918665118e6
Surya Hariharan <140003959+hsuryainfo@users.noreply.github.com>
2024-04-07 13:24:44 -0500

    Add files via upload

---
+++ sonchooser.py
@@ -0,0 +1,8 @@
+def music_picker(genre, mood, tempo):
+    genres = ["rap", "romantic", "hip-hop"]
+    tempo = ["fast", "slow", "very upbeat"]
+    mood = ["happy", "sad"]
+
+    rap_songs = []
+    romantic_songs = []
+    hiphop_songs = []
